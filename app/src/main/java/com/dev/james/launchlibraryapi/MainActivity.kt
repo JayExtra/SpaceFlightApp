ca383b17c2bc067c13f7badf0671ad1f69a9b00f
James Gitonga <40741293+JayExtra@users.noreply.github.com>
2021-09-10 18:36:51 +0300

    App fully setup. Paging , hilt , retrofit all fully implemented. Testing not yet done

--- app/src/main/java/com/dev/james/launchlibraryapi/MainActivity.kt
+++ app/src/main/java/com/dev/james/launchlibraryapi/MainActivity.kt
@@ -2,10 +2,26 @@ package com.dev.james.launchlibraryapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.dev.james.launchlibraryapi.databinding.ActivityMainBinding
+import com.dev.james.launchlibraryapi.databinding.FragmentPreviousLaunchesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment : NavHostFragment
+    private lateinit var controller : NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_LaunchLibraryAPI)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        controller = navHostFragment.navController
+
     }
+
 }
